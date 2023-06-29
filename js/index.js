a82eeca398850eafed49af3210b2521c20780587
Sesshyfan <victoria11235@hotmail.com>
2023-06-28 23:10:36 -0300

    animações e informações de localização e episódios

--- js/index.js
+++ js/index.js
@@ -1,164 +1,106 @@
-// CONFIGURACAO DO CLIENT HTTP AXIOS
+
 const api = axios.create({
     baseURL: 'https://rickandmortyapi.com/api'
 });
 
-// CAPTURAR OS ELEMENTOS DA DOM QUE SERÃO MODIFICADOS PELO JS
 const espacoCardsRow = document.getElementById('espaco-cards')
 const botaoPrev = document.getElementById('botao-prev')
 const botaoAtual = document.getElementById('botao-atual')
 const botaoNext = document.getElementById('botao-next')
 const qtdPersonagensSpan = document.getElementById('qtd-personagens')
+const qtdEpisodiosSpan = document.getElementById('qtd-episodios')
+const qtdLocalizacoesSpan = document.getElementById('qtd-localizacoes')
 
 let paginaAtual = 1
 let totalPaginas = 0
 
-// 1 BUSCA DEVE OCORRER QUANDO A PAGINA CARREGA
 document.addEventListener('DOMContentLoaded', async () => {
     const dadosRetornados = await buscarPersonagens(paginaAtual);
-
-    console.log(dadosRetornados);
+    const episodios = await buscarNumeroEpisodios();
+    const localizacoes = await buscarLocalizacoes()
 
     qtdPersonagensSpan.innerText = dadosRetornados.totalPersonagens
-   
+    qtdEpisodiosSpan.innerText = episodios.data.info.count;
+    qtdLocalizacoesSpan.innerText = localizacoes.data.info.count;
+
     montarColunasCards(dadosRetornados.personagens)
     mudarBotoes(dadosRetornados.paginaAnterior, dadosRetornados.proximaPagina)
-
+    
 });
 
 botaoNext.addEventListener('click', proximaPagina)
 botaoPrev.addEventListener('click', paginaAnterior)
 
 
-
-
 function montarColunasCards(listaPersonagens) {
     espacoCardsRow.innerHTML = ""
 
     listaPersonagens.forEach(async(personagem) => {
-        /*
-            <div class="col-12 col-md-6 col-lg-4">
-                    <div class="card w-100">
-                        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" class="card-img-top"
-                            alt="Avatar">
-                        <div class="card-body px-4">
-                            <h5 class="card-title">
-                                Nome Personagem
-                            </h5>
-
-                            <p class="card-text">
-                                <span class="text-danger">
-                                    <i class="bi bi-caret-right-fill"></i>
-                                </span>
-                                Vivo - Humano
-                            </p>
-
-                            <dl>
-                                <dt>Última localização conhecida:</dt>
-                                <dd>Planeta XPTO</dd>
-
-                                <dt>Visto a última vez em:</dt>
-                                <dd>Nome do Capitulo</dd>
-                            </dl>
-
-                        </div>
-                    </div>
-                </div>
-        */
-
-        // CRIA COLUNA
+       
         const divCol = document.createElement('div')
         divCol.setAttribute('class', 'col-12 col-md-6 col-lg-4 espaco-card')
         divCol.onclick = () => detalhesPersonagem(personagem.id);
 
-        // CRIA CARD
         const divCard = document.createElement('div')
         divCard.setAttribute('class', 'card w-100 rounded-4 shadow')
 
-        // CRIA IMAGEM
         const imgCard = document.createElement('img')
         imgCard.setAttribute('src', `${personagem.image}`)
         imgCard.setAttribute('class', 'card-img-top rounded-top-4')
         imgCard.setAttribute('alt', `${personagem.name}`)
 
-        // CRIA CARD BODY
         const divCardBody = document.createElement('div')
         divCardBody.setAttribute('class', 'card-body px-4')
 
-        // CRIA TITULO CARD
         const tituloCard = document.createElement('h5')
         tituloCard.setAttribute('class', 'card-title')
         tituloCard.innerText = personagem.name
+        tituloCard.classList.add("character-name");
 
-        // CRIA PARAGRAFO
-        /*const descricaoPersonagem = document.createElement('p')
+        const descricaoPersonagem = document.createElement('p')
         descricaoPersonagem.setAttribute('class', 'card-text')
-        descricaoPersonagem.innerHTML = `
-            <span class="${personagem.status === 'Alive' ? 'text-success' : personagem.status === 'Dead' ? 'text-danger' : 'text-secondary'}">
-                <i class="bi bi-caret-right-fill"></i>
-            </span>
-            Vivo - Humano
-        `
-
-        // CRIA DL
+ 
+        switch (personagem.status) {
+            case 'Alive':
+                descricaoPersonagem.innerHTML = `
+                    <span class="text-success">
+                        <i class="bi bi-caret-right-fill"></i>
+                    </span>
+                    Vivo - ${personagem.species}
+                `
+                break;
+ 
+            case 'Dead':
+                descricaoPersonagem.innerHTML = `
+                    <span class="text-danger">
+                        <i class="bi bi-caret-right-fill"></i>
+                    </span>
+                    Morto - ${personagem.species}
+                `
+                break;
+ 
+            default:
+                descricaoPersonagem.innerHTML = `
+                    <span class="text-secondary">
+                        <i class="bi bi-caret-right-fill"></i>
+                    </span>
+                    Desconhecido - ${personagem.species}
+                `
+        }
+ 
+        const dadosEpisodio = await buscarDadosEpisodio(personagem.episode[personagem.episode.length - 1])
         const detalhamentoPersonagem = document.createElement('dl');
         detalhamentoPersonagem.innerHTML = `
             <dt>Última localização conhecida:</dt>
-            <dd>Planeta XPTO</dd>
-
+            <dd>${personagem.location.name}</dd>
+ 
             <dt>Visto a última vez em:</dt>
-            <dd>Nome do Capitulo</dd>
-        `*/
+            <dd>${dadosEpisodio.nomeEpisodio}</dd>
+ 
+            <dt>Foi ao ar em:</dt>
+            <dd>${dadosEpisodio.dataLancamento}</dd>
+        `
 
-         // CRIA PARAGRAFO
-         const descricaoPersonagem = document.createElement('p')
-         descricaoPersonagem.setAttribute('class', 'card-text')
- 
-         switch (personagem.status) {
-             case 'Alive':
-                 descricaoPersonagem.innerHTML = `
-                     <span class="text-success">
-                         <i class="bi bi-caret-right-fill"></i>
-                     </span>
-                     Vivo - ${personagem.species}
-                 `
-                 break;
- 
-             case 'Dead':
-                 descricaoPersonagem.innerHTML = `
-                     <span class="text-danger">
-                         <i class="bi bi-caret-right-fill"></i>
-                     </span>
-                     Morto - ${personagem.species}
-                 `
-                 break;
- 
-             default:
-                 descricaoPersonagem.innerHTML = `
-                     <span class="text-secondary">
-                         <i class="bi bi-caret-right-fill"></i>
-                     </span>
-                     Desconhecido - ${personagem.species}
-                 `
-         }
- 
- 
-         // CRIA DL
-         const dadosEpisodio = await buscarDadosEpisodio(personagem.episode[personagem.episode.length - 1])
- 
-         const detalhamentoPersonagem = document.createElement('dl');
-         detalhamentoPersonagem.innerHTML = `
-             <dt>Última localização conhecida:</dt>
-             <dd>${personagem.location.name}</dd>
- 
-             <dt>Visto a última vez em:</dt>
-             <dd>${dadosEpisodio.nomeEpisodio}</dd>
- 
-             <dt>Foi ao ar em:</dt>
-             <dd>${dadosEpisodio.dataLancamento}</dd>
-         `
-
-        // APPENDS - criar os filhos dentros dos respectivos elementos pais
         divCardBody.appendChild(tituloCard)
         divCardBody.appendChild(descricaoPersonagem)
         divCardBody.appendChild(detalhamentoPersonagem)
@@ -193,6 +135,32 @@ function mudarBotoes(prev, next) {
     }
 }
 
+async function buscarNumeroEpisodios() {
+    try {
+        const resposta = await api.get('/episode');
+        const dadosEpisodios = resposta;
+        
+        return dadosEpisodios;
+
+    } catch {
+        alert("Deu algo errado na busca dos dados dos episódios.")
+    }
+}
+
+async function buscarLocalizacoes() {
+    try {
+        const resposta = await api.get('/location');
+
+        const dadosLocalizacao = resposta;
+        
+        return dadosLocalizacao
+
+    } catch {
+        alert("Deu algo errado na busca do número dos dados de localização.")
+    }
+}
+
+
 async function buscarPersonagens(pagina) {
     try {
         const resposta = await api.get('/character', {
@@ -213,9 +181,7 @@ async function buscarPersonagens(pagina) {
        
 
     } catch (erro) {
-        console.log(erro) // debug (erros de requisicao e erros de sintaxe - 400, 401, 500 ...
         alert('Erro na busca de personagens.')
-        // aqui é momento de mostrar uma mensagem se der erro na requisicao
     }
 }
 
@@ -223,7 +189,6 @@ async function buscarDadosEpisodio(url) {
     try {
         const resposta = await axios.get(url)
 
-        // resposta.data
         return {
             id: resposta.data.id,
             nomeEpisodio: resposta.data.name,
@@ -241,9 +206,9 @@ function detalhesPersonagem(personagemId) {
 }
 
 async function proximaPagina() {
-    // verificar se o botão esta desabilitado
+    
     if (!botaoNext.classList.contains('disabled')) {
-        // só dispara a requisição pra API se o botão estiver habilitado
+        
         ++paginaAtual
 
         const dadosAPI = await buscarPersonagens(paginaAtual)
@@ -255,9 +220,9 @@ async function proximaPagina() {
 }
 
 async function paginaAnterior() {
-    // verificar se o botão esta desabilitado
+    
     if (!botaoPrev.classList.contains('disabled')) {
-        // só dispara a requisição pra API se o botão estiver habilitado
+       
         --paginaAtual
 
         const dadosAPI = await buscarPersonagens(paginaAtual)
--- js/personagens.js
+++ js/personagens.js
@@ -2,159 +2,107 @@ const api = axios.create({
     baseURL: 'https://rickandmortyapi.com/api'
 });
 
-// CAPTURAR OS ELEMENTOS DA DOM QUE SERÃO MODIFICADOS PELO JS
 const espacoCardsRow = document.getElementById('espaco-cards')
 const qtdPersonagensSpan = document.getElementById('qtd-personagens')
+const qtdEpisodiosSpan = document.getElementById('qtd-episodios')
+const qtdLocalizacoesSpan = document.getElementById('qtd-localizacoes')
 
-// 1 BUSCA DEVE OCORRER QUANDO A PAGINA CARREGA
 document.addEventListener('DOMContentLoaded', async () => {
 
     const personagemId = localStorage.getItem('personagemId');
 
     const dadosRetornados = await buscarPersonagens(personagemId);
+    const numeroPersonagens = await buscarNumeroPersonagens();
+    const episodios = await buscarNumeroEpisodios();
+    const localizacoes = await buscarLocalizacoes()
 
     console.log(dadosRetornados);
 
-    qtdPersonagensSpan.innerText = dadosRetornados.totalPersonagens
+    qtdPersonagensSpan.innerText = numeroPersonagens.data.info.count;
+    qtdEpisodiosSpan.innerText = episodios.data.info.count;
+    qtdLocalizacoesSpan.innerText = localizacoes.data.info.count;
    
-    montarColunasCards(dadosRetornados)
+    montarColunasCards(dadosRetornados);
 
 });
 
-function montarColunasCards(personagem) {
+async function montarColunasCards(personagem) {
     espacoCardsRow.innerHTML = ""
-        /*
-            <div class="col-12 col-md-6 col-lg-4">
-                    <div class="card w-100">
-                        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" class="card-img-top"
-                            alt="Avatar">
-                        <div class="card-body px-4">
-                            <h5 class="card-title">
-                                Nome Personagem
-                            </h5>
+       
+        const divCol = document.createElement('div');
+        divCol.setAttribute('class', 'col-12 col-md-6 col-lg-4 espaco-card');
 
-                            <p class="card-text">
-                                <span class="text-danger">
-                                    <i class="bi bi-caret-right-fill"></i>
-                                </span>
-                                Vivo - Humano
-                            </p>
+        const divCard = document.createElement('div');
+        divCard.setAttribute('class', 'card w-100 rounded-4 shadow');
 
-                            <dl>
-                                <dt>Última localização conhecida:</dt>
-                                <dd>Planeta XPTO</dd>
+        const imgCard = document.createElement('img');
+        imgCard.setAttribute('src', `${personagem.image}`);
+        imgCard.setAttribute('class', 'card-img-top rounded-top-4');
+        imgCard.setAttribute('alt', `${personagem.name}`);
 
-                                <dt>Visto a última vez em:</dt>
-                                <dd>Nome do Capitulo</dd>
-                            </dl>
+        const divCardBody = document.createElement('div');
+        divCardBody.setAttribute('class', 'card-body px-4');
 
-                        </div>
-                    </div>
-                </div>
-        */
+        const tituloCard = document.createElement('h5');
+        tituloCard.setAttribute('class', 'card-title');
+        tituloCard.innerText = personagem.name;
+        tituloCard.classList.add("character-name");
 
-        // CRIA COLUNA
-        const divCol = document.createElement('div')
-        divCol.setAttribute('class', 'col-12 col-md-6 col-lg-4 espaco-card')
-
-        // CRIA CARD
-        const divCard = document.createElement('div')
-        divCard.setAttribute('class', 'card w-100 rounded-4 shadow')
-
-        // CRIA IMAGEM
-        const imgCard = document.createElement('img')
-        imgCard.setAttribute('src', `${personagem.image}`)
-        imgCard.setAttribute('class', 'card-img-top rounded-top-4')
-        imgCard.setAttribute('alt', `${personagem.name}`)
-
-        // CRIA CARD BODY
-        const divCardBody = document.createElement('div')
-        divCardBody.setAttribute('class', 'card-body px-4')
-
-        // CRIA TITULO CARD
-        const tituloCard = document.createElement('h5')
-        tituloCard.setAttribute('class', 'card-title')
-        tituloCard.innerText = personagem.name
-
-        // CRIA PARAGRAFO
-        /*const descricaoPersonagem = document.createElement('p')
-        descricaoPersonagem.setAttribute('class', 'card-text')
-        descricaoPersonagem.innerHTML = `
-            <span class="${personagem.status === 'Alive' ? 'text-success' : personagem.status === 'Dead' ? 'text-danger' : 'text-secondary'}">
-                <i class="bi bi-caret-right-fill"></i>
-            </span>
-            Vivo - Humano
-        `
-
-        // CRIA DL
+        const descricaoPersonagem = document.createElement('p');
+        descricaoPersonagem.setAttribute('class', 'card-text');
+ 
+        switch (personagem.status) {
+            case 'Alive':
+                descricaoPersonagem.innerHTML = `
+                    <span class="text-success">
+                        <i class="bi bi-caret-right-fill"></i>
+                    </span>
+                    Vivo - ${personagem.species}
+                `
+                break;
+ 
+            case 'Dead':
+                descricaoPersonagem.innerHTML = `
+                    <span class="text-danger">
+                        <i class="bi bi-caret-right-fill"></i>
+                    </span>
+                    Morto - ${personagem.species}
+                `
+                break;
+ 
+            default:
+                descricaoPersonagem.innerHTML = `
+                    <span class="text-secondary">
+                        <i class="bi bi-caret-right-fill"></i>
+                    </span>
+                    Desconhecido - ${personagem.species}
+                `
+        }
+ 
+        const dadosEpisodio = await buscarDadosEpisodio(personagem.episode[personagem.episode.length - 1])
+ 
         const detalhamentoPersonagem = document.createElement('dl');
         detalhamentoPersonagem.innerHTML = `
             <dt>Última localização conhecida:</dt>
-            <dd>Planeta XPTO</dd>
-
+            <dd>${personagem.location.name}</dd>
+ 
             <dt>Visto a última vez em:</dt>
-            <dd>Nome do Capitulo</dd>
-        `*/
+            <dd>${dadosEpisodio.nomeEpisodio}</dd>
+ 
+            <dt>Foi ao ar em:</dt>
+            <dd>${dadosEpisodio.dataLancamento}</dd>
+        `
 
-         // CRIA PARAGRAFO
-         const descricaoPersonagem = document.createElement('p')
-         descricaoPersonagem.setAttribute('class', 'card-text')
- 
-         switch (personagem.status) {
-             case 'Alive':
-                 descricaoPersonagem.innerHTML = `
-                     <span class="text-success">
-                         <i class="bi bi-caret-right-fill"></i>
-                     </span>
-                     Vivo - ${personagem.species}
-                 `
-                 break;
- 
-             case 'Dead':
-                 descricaoPersonagem.innerHTML = `
-                     <span class="text-danger">
-                         <i class="bi bi-caret-right-fill"></i>
-                     </span>
-                     Morto - ${personagem.species}
-                 `
-                 break;
- 
-             default:
-                 descricaoPersonagem.innerHTML = `
-                     <span class="text-secondary">
-                         <i class="bi bi-caret-right-fill"></i>
-                     </span>
-                     Desconhecido - ${personagem.species}
-                 `
-         }
- 
- 
-         // CRIA DL
-         const dadosEpisodio = buscarDadosEpisodio(personagem.episode[personagem.episode.length - 1])
- 
-         const detalhamentoPersonagem = document.createElement('dl');
-         detalhamentoPersonagem.innerHTML = `
-             <dt>Última localização conhecida:</dt>
-             <dd>${personagem.location.name}</dd>
- 
-             <dt>Visto a última vez em:</dt>
-             <dd>${dadosEpisodio.nomeEpisodio}</dd>
- 
-             <dt>Foi ao ar em:</dt>
-             <dd>${dadosEpisodio.dataLancamento}</dd>
-         `
+        divCardBody.appendChild(tituloCard);
+        divCardBody.appendChild(descricaoPersonagem);
+        divCardBody.appendChild(detalhamentoPersonagem);
 
-        // APPENDS - criar os filhos dentros dos respectivos elementos pais
-        divCardBody.appendChild(tituloCard)
-        divCardBody.appendChild(descricaoPersonagem)
-        divCardBody.appendChild(detalhamentoPersonagem)
+        divCard.appendChild(imgCard);
+        divCard.appendChild(divCardBody);
 
-        divCard.appendChild(imgCard)
-        divCard.appendChild(divCardBody)
+        divCol.appendChild(divCard);
 
-        divCol.appendChild(divCard)
-
-        espacoCardsRow.appendChild(divCol)
+        espacoCardsRow.appendChild(divCol);
 
 }
 
@@ -169,9 +117,50 @@ async function buscarPersonagens(personagemId) {
        
 
     } catch (erro) {
-        console.log(erro) // debug (erros de requisicao e erros de sintaxe - 400, 401, 500 ...
+        console.log(erro) 
         alert('Erro na busca de personagens.')
-        // aqui é momento de mostrar uma mensagem se der erro na requisicao
+    }
+}
+
+async function buscarNumeroEpisodios() {
+    try {
+        const resposta = await api.get('/episode');
+        const dadosEpisodios = resposta;
+        
+        return dadosEpisodios;
+
+    } catch (erro) {
+        console.log(erro) 
+        alert("Deu algo errado na busca de dados dos episódios.")
+    }
+}
+
+async function buscarLocalizacoes() {
+    try {
+        const resposta = await api.get('/location');
+
+        const dadosLocalizacao = resposta;
+        
+        return dadosLocalizacao
+
+    } catch(erro) {
+        console.log(erro)
+        alert("Deu algo errado na busca dos dados de localização.")
+    }
+}
+
+async function buscarNumeroPersonagens() {
+    try {
+        const resposta = await api.get('/character');
+
+        const InfoPersonagens = resposta;
+       
+        return InfoPersonagens;
+       
+
+    } catch (erro) {
+        console.log(erro) 
+        alert('"Deu algo errado na busca dos dados dos personagens."')
     }
 }
 
@@ -179,14 +168,13 @@ async function buscarDadosEpisodio(url) {
     try {
         const resposta = await axios.get(url)
 
-        // resposta.data
         return {
             id: resposta.data.id,
             nomeEpisodio: resposta.data.name,
             dataLancamento: resposta.data.air_date
         }
     } catch {
-        alert("Deu algo errado na busca do episódio")
+        alert("Deu algo errado na busca do episódio.")
     }
 }
 
